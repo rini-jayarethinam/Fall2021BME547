b438f5dd773836aa9d95eaa5c7f8d78743caa838
rinishagai@gmail.com <rinishagai@gmail.com>
2021-09-13 19:11:49 -0400

    Defined the point_inputs function to collect values for the first point, second point, and the new x-value from the user

---
+++ coordinates_classwork.py
@@ -0,0 +1,12 @@
+# Coordinates Classwork by Rini Jayarethinam
+
+def point_inputs():
+    print("\nEnter the first point")
+    first_point = input("Input the first point x and y values separated by a comma: ")
+    print("\nEnter the second point")
+    second_point = input("Input the second point x and y values separated by a comma: ")
+    x_value = input("\nInput the x-value: ")
+    return first_point, second_point, x_value
+    
+if __name__ == "__main__":
+    point_inputs()
