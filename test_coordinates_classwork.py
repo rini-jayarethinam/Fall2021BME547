f2024fa66229d297244f226f0027564a8ac16485
rinishagai@gmail.com <rinishagai@gmail.com>
2021-09-13 23:25:43 -0400

    added 2 more test cases to y-value calculation test function to have a total of 3 test cases

--- test_coordinates_classwork.py
+++ test_coordinates_classwork.py
@@ -6,7 +6,7 @@ def test_slope_calculation(x1_value, y1_value, x2_value, y2_value, expected_m):
     answer = slope_calculation(x1_value, y1_value, x2_value, y2_value)
     assert answer == expected_m
     
-@pytest.mark.parametrize("slope_m, new_x_value, x1_value, y1_value, expected_y", [(1,5,1,2,6)])
+@pytest.mark.parametrize("slope_m, new_x_value, x1_value, y1_value, expected_y", [(1,5,1,2,6),(1,5,1,2,8),(1,7,1,2,8)])
 def test_y_value_calculation(slope_m, new_x_value, x1_value, y1_value, expected_y):
     from coordinates_classwork import new_y_value_calculation
     answer = new_y_value_calculation(slope_m, new_x_value, x1_value, y1_value)
