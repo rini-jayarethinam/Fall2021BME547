0be63ee468825dbb2f8c5e0aa22de0373a360de5
rinishagai@gmail.com <rinishagai@gmail.com>
2021-09-13 21:59:20 -0400

    added code to input conversion function to change inputted x-value into an integer

--- coordinates_classwork.py
+++ coordinates_classwork.py
@@ -8,17 +8,18 @@ def point_inputs():
     new_x_value = input("\nInput the x-value: ")
     return input_points, new_x_value
     
-def input_type_conversion(input_points):
+def input_type_conversion(input_points, new_x_value):
     point_list = list(eval(input_points.replace(',',',').replace(' ',',')))
     x1_value = point_list[0][0]
     y1_value = point_list[0][1]
     x2_value = point_list[1][0]
     y2_value = point_list[1][1]
-    return x1_value, y1_value, x2_value, y2_value
+    new_x_value = int(new_x_value)
+    return x1_value, y1_value, x2_value, y2_value, new_x_value
     
 def coordinates_driver():
     input_points, new_x_value = point_inputs()
-    x1_value, y1_value, x2_value, y2_value = input_type_conversion(input_points)
+    x1_value, y1_value, x2_value, y2_value, new_x_value = input_type_conversion(input_points, new_x_value)
 
 if __name__ == "__main__":
     coordinates_driver()
