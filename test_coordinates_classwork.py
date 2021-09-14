65ae29b28c2a77848348e061b17dc0b16cfea502
rinishagai@gmail.com <rinishagai@gmail.com>
2021-09-13 23:15:21 -0400

    added 2 more test cases to slope calculation test function to have a total of 3 test cases

--- test_coordinates_classwork.py
+++ test_coordinates_classwork.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.mark.parametrize("x1_value, y1_value, x2_value, y2_value, expected_m", [(1,2,3,4,1)])
+@pytest.mark.parametrize("x1_value, y1_value, x2_value, y2_value, expected_m", [(1,2,3,4,1),(1,2,3,4,5),(1,2,3,3,0.5)])
 def test_slope_calculation(x1_value, y1_value, x2_value, y2_value, expected_m):
     from coordinates_classwork import slope_calculation
     answer = slope_calculation(x1_value, y1_value, x2_value, y2_value)
