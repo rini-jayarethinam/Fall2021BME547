0d131a59be9346d78826e7a603ee8146384eeb7b
rinishagai@gmail.com <rinishagai@gmail.com>
2021-09-13 23:19:25 -0400

    defined test function for y_value calcuation using 1 test case

--- test_coordinates_classwork.py
+++ test_coordinates_classwork.py
@@ -5,3 +5,9 @@ def test_slope_calculation(x1_value, y1_value, x2_value, y2_value, expected_m):
     from coordinates_classwork import slope_calculation
     answer = slope_calculation(x1_value, y1_value, x2_value, y2_value)
     assert answer == expected_m
+    
+@pytest.mark.parametrize("slope_m, new_x_value, x1_value, y1_value, expected_y", [(1,5,1,2,6)])
+def test_y_value_calculation(slope_m, new_x_value, x1_value, y1_value, expected_y):
+    from coordinates_classwork import new_y_value_calculation
+    answer = new_y_value_calculation(slope_m, new_x_value, x1_value, y1_value)
+    assert answer == expected_y
