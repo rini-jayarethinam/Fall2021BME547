16b9baac36bcaade26a6423c14b1bc74fbecaa7b
rinishagai@gmail.com <rinishagai@gmail.com>
2021-09-13 23:13:10 -0400

    test function developed for testing slope calculation using 1 test case

---
+++ test_coordinates_classwork.py
@@ -0,0 +1,7 @@
+import pytest
+
+@pytest.mark.parametrize("x1_value, y1_value, x2_value, y2_value, expected_m", [(1,2,3,4,1)])
+def test_slope_calculation(x1_value, y1_value, x2_value, y2_value, expected_m):
+    from coordinates_classwork import slope_calculation
+    answer = slope_calculation(x1_value, y1_value, x2_value, y2_value)
+    assert answer == expected_m
