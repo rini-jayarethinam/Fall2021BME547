5be55d50aa37b14778748c48250b70c41691c06c
rinishagai@gmail.com <rinishagai@gmail.com>
2021-09-13 21:57:41 -0400

    defined function to extract x and y values from converted list of coordinates

--- coordinates_classwork.py
+++ coordinates_classwork.py
@@ -7,3 +7,18 @@ def point_inputs():
     input_points = input("Input the 2 coordinates here: ")
     new_x_value = input("\nInput the x-value: ")
     return input_points, new_x_value
+    
+def input_type_conversion(input_points):
+    point_list = list(eval(input_points.replace(',',',').replace(' ',',')))
+    x1_value = point_list[0][0]
+    y1_value = point_list[0][1]
+    x2_value = point_list[1][0]
+    y2_value = point_list[1][1]
+    return x1_value, y1_value, x2_value, y2_value
+    
+def coordinates_driver():
+    input_points, new_x_value = point_inputs()
+    x1_value, y1_value, x2_value, y2_value = input_type_conversion(input_points)
+
+if __name__ == "__main__":
+    coordinates_driver()
