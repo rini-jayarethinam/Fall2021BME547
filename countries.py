cea930a4667358074b64538847fd0ac21592fecf
rinishagai@gmail.com <rinishagai@gmail.com>
2021-10-11 12:54:47 -0400

    countries was added on 10/11

---
+++ countries.py
@@ -0,0 +1,11 @@
+import requests
+
+server_name = "http://vcm-7631.vm.duke.edu:5000/"
+
+r = requests.get(server_name+"countries")
+print(r.text)
+
+request_json = {"one": "Afghanistan", "two": "Albania"}
+r = requests.post(server_name+"compare", json = request_json)
+print(r.text)
+print(r.json())
